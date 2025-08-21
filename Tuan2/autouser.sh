33b26c2a820211f66d3511e1640a67f864fcc22c
unknown <hdl243004@gmail.com>
2025-08-21 14:39:54 +0700

    Tuan2

--- Tuan2/autouser.sh
+++ Tuan2/autouser.sh
@@ -4,4 +4,4 @@ read -p "Enter new username: " username
 read -s -p "Enter new password: " password
 echo
 sudo useradd -m "$username"
-echo "$username:$password" | sudo chpasswd
+echo "$username:$password" | sudo passwd
